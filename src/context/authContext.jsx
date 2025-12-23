c6e85b1912fc5b1e6a0f83f619700f5e4ccbc9c9
Abdullahsadiqi344 <abdullahsadiqi698@gmail.com>
2025-12-24 00:59:02 +0500

    fix: added withCredentials to axios defaults

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -8,7 +8,8 @@ export const AuthDataContext=createContext()
 
 const AuthContext = ( {children}) => {
 
-   const serverUrl = import.meta.env.VITE_BACKEND_URL  
+   //const serverUrl = import.meta.env.VITE_BACKEND_URL  
+   const serverUrl =  "http://localhost:60001"
     let value={
         serverUrl,
     }
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import App from './App.jsx'
 import AuthContext from './context/authContext.jsx'
 import Usercontext from './context/Usercontext.jsx'
 import { CartProvider } from './context/cartcontext.jsx'
-
+import axios from 'axios';
+axios.defaults.withCredentials = true;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
